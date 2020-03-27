e0a43fbf11da086271a5afc969015b22431d9336
BrahimLamkadem2 <60020813+BrahimLamkadem2@users.noreply.github.com>
2020-03-27 14:46:24 +0100

    Updates

---
+++ precache-manifest.15f298e356c6420104e9737332507849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d24c32fa37017378de56cd0e1599d96",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3c8cf6140232ad4ad16d",
+    "url": "/todolist/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3c8cf6140232ad4ad16d",
+    "url": "/todolist/static/js/2.f3539973.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.f3539973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132aaec72301009be562",
+    "url": "/todolist/static/js/main.3475ad2b.chunk.js"
+  },
+  {
+    "revision": "5dc327340e8e32fc3756",
+    "url": "/todolist/static/js/runtime-main.c337c584.js"
+  }
+]);
